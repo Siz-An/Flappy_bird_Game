1c8ed5b4ba8d7def2f6b892b3935f7420d23dca9
Sizan <mahatosizan@gmail.com>
2024-07-12 21:46:56 +0545

    images added for game

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'Game/flappy_bird_game.dart';
 
 void main() {
 
---
+++ lib/utils/imageString.dart
@@ -0,0 +1,18 @@
+class ImageString{
+
+  static const background = 'background.png';
+  static const ground = 'background.png';
+  static const clouds = 'background.png';
+  static const pipe = 'background.png';
+  static const pipeDown = 'background.png';
+
+  static const birdMidFlip = 'background.png';
+  static const birdUpFlip = 'background.png';
+  static const birdDownFlip = 'background.png';
+
+  static const gameOver = 'background.png';
+  static const menu = 'background.png';
+  static const message = 'background.png';
+
+
+}
